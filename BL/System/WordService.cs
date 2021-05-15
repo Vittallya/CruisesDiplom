1330a03dcf04a3cdfb0dfce7534051ca0a813d9e
Vitaliy <Vit.Por@bk.ru>
2021-05-15 11:01:47 +0300

    Ворд теперь выводит корректно коллекции любого количества символов

--- BL/System/WordService.cs
+++ BL/System/WordService.cs
@@ -3,6 +3,7 @@ using DAL.Dto;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using word = Microsoft.Office.Interop.Word;
@@ -15,11 +16,13 @@ namespace BL
         public void ShowOrderContract(OrderDto orderDto, ValuteGetterService valuteGetter, string path)
         {
             Dictionary<string, string> stubs = new Dictionary<string, string>();
+            Dictionary<string, string[]> stubsColl = new Dictionary<string, string[]>();
 
 
             DateTime endDate = orderDto.TourDto.StartDate.AddDays(orderDto.TourDto.DaysCount);
             StringBuilder insStr = new StringBuilder();
-            StringBuilder placementsStr = new StringBuilder();
+            List<string> plList = new List<string>();
+            List<string> insList = new List<string>();
             
 
             stubs.Add("[Номер_заказа]", orderDto.Id.ToString());
@@ -32,40 +35,47 @@ namespace BL
             stubs.Add("[Кол-во_дней]", orderDto.TourDto.DaysCount.ToString());
             stubs.Add("[Кол-во_туристов]", orderDto.PeopleCount.ToString());
 
-            placementsStr.AppendLine("Туристы:");
 
-            foreach (var pl in orderDto.PlacementDtos)
+            var list = orderDto.PlacementDtos.ToList();
+            for (int i = 0; i < list.Count; i++)
             {
+                var pl = list[i];
+
                 string doc = !pl.IsChildBefore14 ? pl.Pasport : pl.BirthDoc;
-                string docName = !pl.IsChildBefore14 ? "п-т" : "св-во. рожд.";
-                placementsStr.AppendLine($"{pl.Fio}, {docName}: {doc}, к-та №: {pl.CabinDto.Id}, п: {pl.CabinDto.Deck}");
+                string docName = !pl.IsChildBefore14 ? "паспорт" : "св-во. рождении";
+
+                string line = $"{i+1}){pl.Fio}, {docName}: {doc}, каюта №: {pl.CabinDto.Id}, палуба: {pl.CabinDto.Deck}";
+                plList.Add(line);
             }
+            stubsColl.Add("[Туристы]", plList.ToArray());
 
 
             if (orderDto.HasIns)
             {
-                insStr.AppendLine("Страховка: ");
-                foreach (var ins in orderDto.InsuranceDtos)
+                var list1 = orderDto.InsuranceDtos.ToList();
+
+                for (int i = 0; i < orderDto.InsuranceDtos.Count; i++)
                 {
-                    insStr.AppendLine(ins.Name);
+                    var ins = list1[i];
+                    string line = $"{i + 1}) {ins.Name}";
+                    insList.Add(line);
                 }
-                stubs.Add("[Страховка]", insStr.ToString());
+
             }
             else 
-                stubs.Add("[Страховка]", "нет");
+                insList.Add("нет");
 
-
-            stubs.Add("[Туристы]", placementsStr.ToString());
+            stubsColl.Add("[Страховка]", insList.ToArray());
 
             stubs.Add("[Общая_стоимость]", orderDto.FullCost.ToString("0.##"));
             stubs.Add("[Общая_стоимостьUSD]", valuteGetter.GetUSDValue(orderDto.FullCost).ToString("0.##"));
             stubs.Add("[Общая_стоимостьEUR]", valuteGetter.GetEuroValue(orderDto.FullCost).ToString("0.##"));
 
-            ShowDocument(stubs, path);
+            ShowDocument(stubs, path, stubsColl);
 
         }
         public const int CUT_PART = 8;
-        private bool ShowDocument(Dictionary<string, string> stubs, string path)
+        private bool ShowDocument(Dictionary<string, string> stubs, string path, Dictionary<string, string[]> stubsCollection = null)
         {
             var wordApp = new word.Application();
             try
@@ -81,27 +91,33 @@ namespace BL
                 //newDoc.Content.PageSetup.RightMargin = 56.7f;
 
                 foreach ( var stub in stubs )
-                {
-                    //if (stub.Value?.Length > 30)
-                    //{
-                    //    int cuttedL = stub.Value.Length / CUT_PART;
-
-                    //    int ost = stub.Value.Length % CUT_PART;
-
-                    //    string part = stub.Value.Substring(0, cuttedL + ost);
-                    //    ReplaceWordStub($"{stub}1", part, newDoc);
-
-                    //    for (int i = 1; i < CUT_PART; i++)
-                    //    {
-                    //        part = stub.Value.Substring(i * cuttedL, cuttedL);
-                    //        ReplaceWordStub($"{stub}{i + 1}", part, newDoc);
-                    //    }
-                    //}
-                    //else
-                    //{
-                        ReplaceWordStub(stub.Key, stub.Value, newDoc);
-                    //}
+                {   
+                    ReplaceWordStub(stub.Key, stub.Value, newDoc);                    
                 }
+
+                if(stubsCollection != null)
+                {
+                    foreach(var stub in stubsCollection)
+                    {
+                        for (int i = 0; i < stub.Value.Length; i++)
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            if (i < stub.Value.Length - 1)
+                            {
+                                sb.AppendLine(stub.Value[i]);
+                                sb.Append($"{stub.Key}");
+                            }
+                            else
+                            {
+                                sb.Append(stub.Value[i]);
+                            }
+
+                            ReplaceWordStub(stub.Key, sb.ToString(), newDoc);
+                            sb.Clear();
+                        }
+                    }
+                }
+
                 wordApp.Visible = true;
             }
             catch ( Exception ex )
